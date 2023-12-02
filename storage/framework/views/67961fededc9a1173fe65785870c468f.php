057c18f273c2f1a66ffab0b2fdb81ade90ee9bec
Bijosh Shagar <93047483+Itsbijoshuman@users.noreply.github.com>
2023-12-02 20:30:44 +0530

    Add files via upload

---
+++ storage/framework/views/67961fededc9a1173fe65785870c468f.php
@@ -0,0 +1,11 @@
+//resources/views/dashboard.blade.php
+
+  
+<?php $__env->startSection('title', 'Dashboard - Laravel Admin Panel With Login and Registration by Bijosh'); ?>
+  
+<?php $__env->startSection('contents'); ?>
+  <div class="row">
+    Dashboard
+  </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /opt/lampp/htdocs/edu/resources/views/dashboard.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/b95cab4f138a8301b596735c5765f78d.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+      <div class="copyright text-center my-auto">
+        <span>Copyright © Bijosh's WEbsite 2023</span>
+      </div>
+    </div>
+  </footer><?php /**PATH /opt/lampp/htdocs/edu/resources/views/layouts/footer.blade.php ENDPATH**/ ?>
